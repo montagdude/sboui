2ae412c8a53dc6d094a9f705314be2bb8f66ac2b
Daniel Prosser <dpross1100@msn.com>
2016-12-31 07:52:05 -0500

    Put signals in their own namespace

---
+++ include/signals.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+namespace signals 
+{
+  extern std::string resizeSignal;
+  extern std::string quitSignal;
+  extern std::string highlightSignal;
+  extern std::string keyTabSignal;
+}
---
+++ src/cpp/signals.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include "signals.h"
+
+namespace signals
+{
+  std::string resizeSignal = "__RESIZE__";
+  std::string quitSignal = "__QUIT__";
+  std::string highlightSignal = "__HIGHLIGHT__";
+  std::string keyTabSignal = "__TAB__";
+}
