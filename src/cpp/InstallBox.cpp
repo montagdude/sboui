montagdude/sboui

commit 075652a1641490da81a02460149f17dc64f26cc9

Working Remove action

src/cpp/InstallBox.cpp
@@ -289,7 +289,7 @@ void InstallBox::preferredSize(int & height, int & width) const
 
 bool InstallBox::installingAllDeps() const
 {
-  unsigned int nreqs, i;
+  int nreqs, i;
 
   nreqs = _items.size() - 1;   // Only consider dependencies, not the requested
                                // SlackBuild
@@ -317,31 +317,37 @@ int InstallBox::create(BuildListItem & build,
   unsigned int nreqs, i;
   std::vector<BuildListItem *> reqlist;
 
-  check = compute_reqs_order(build, reqlist, slackbuilds);
-  if (check != 0) { return check; }
+  check = 0;
+  nreqs = 0;
 
-  // Create copy of reqlist and determine action
+  if (action != "Remove")
+  {
+    check = compute_reqs_order(build, reqlist, slackbuilds);
+    if (check != 0) { return check; }
 
-  nreqs = reqlist.size();
-  for ( i = 0; i < nreqs; i++ ) 
-  { 
-    _builds.push_back(*reqlist[i]); 
-    if (! reqlist[i]->getBoolProp("installed"))
-    {
-      _builds[i].setBoolProp("tagged", true);
-      _builds[i].addProp("action", "Install");
-    }
-    else
-    {
-      if (reqlist[i]->upgradable())
+    // Create copy of reqlist and determine action
+
+    nreqs = reqlist.size();
+    for ( i = 0; i < nreqs; i++ ) 
+    { 
+      _builds.push_back(*reqlist[i]); 
+      if (! reqlist[i]->getBoolProp("installed"))
       {
         _builds[i].setBoolProp("tagged", true);
-        _builds[i].addProp("action", "Upgrade");
+        _builds[i].addProp("action", "Install");
       }
       else
       {
-        _builds[i].setBoolProp("tagged", false);
-        _builds[i].addProp("action", "Reinstall");
+        if (reqlist[i]->upgradable())
+        {
+          _builds[i].setBoolProp("tagged", true);
+          _builds[i].addProp("action", "Upgrade");
+        }
+        else
+        {
+          _builds[i].setBoolProp("tagged", false);
+          _builds[i].addProp("action", "Reinstall");
+        }
       }
     }
   }
@@ -359,10 +365,14 @@ int InstallBox::create(BuildListItem & build,
 
   // Set window title
 
-  if (nreqs == 1)
-    setName(build.name() + " (1 dep)");
-  else
-    setName(build.name() + " (" + int2string(nreqs) + " deps)");
+  if (action != "Remove")
+  {
+    if (nreqs == 1)
+      setName(build.name() + " (1 dep)");
+    else
+      setName(build.name() + " (" + int2string(nreqs) + " deps)");
+  }
+  else { setName("Remove " + build.name() + "?"); }
 
   return check;
 }
@@ -478,7 +488,7 @@ std::string InstallBox::exec()
 
 /*******************************************************************************
 
-Install, upgrade, or reinstall SlackBuild and dependencies. Returns 0 on
+Install, upgrade, reinstall, or remove SlackBuild and dependencies. Returns 0 on
 success.
 
 *******************************************************************************/
@@ -488,7 +498,7 @@ int InstallBox::applyChanges() const
   int check, retval;
   std::string response, msg;
 
-  // Install/upgrade/reinstall tagged SlackBuilds
+  // Install/upgrade/reinstall/remove tagged SlackBuilds
 
   nbuilds = _builds.size();
   retval = 0;
@@ -498,6 +508,8 @@ int InstallBox::applyChanges() const
     {
       if (_builds[i].getProp("action") == "Upgrade") { check = 
                                                upgrade_slackbuild(_builds[i]); }
+      else if (_builds[i].getProp("action") == "Remove") { check = 
+                                                remove_slackbuild(_builds[i]); }
       else { check = install_slackbuild(_builds[i]); }
 
       // Handle errors

src/cpp/MainWindow.cpp
@@ -631,22 +631,30 @@ bool MainWindow::modifyPackage(BuildListItem & build,
   unsigned int ninstaller;
   InstallBox installer;
 
-  printStatus("Computing dependencies for " + build.name() + " ...");
-  check = installer.create(build, _slackbuilds, action);
+  if (action != "Remove")
+  {
+    printStatus("Computing dependencies for " + build.name() + " ...");
+    check = installer.create(build, _slackbuilds, action);
 
 //FIXME: Make some sort of error message class to show this
-  if (check != 0) 
-  { 
-    printStatus("Some requirements of " + build.name() +
-                " not found in repository."); 
-    return false;
-  }
+    if (check != 0) 
+    { 
+      printStatus("Some requirements of " + build.name() +
+                  " not found in repository."); 
+      return false;
+    }
 
-  ninstaller = installer.numItems();
-  if (ninstaller == 2) { printStatus(
+    ninstaller = installer.numItems();
+    if (ninstaller == 2) { printStatus(
                                     "1 dependency for " + build.name() + "."); }
-  else { printStatus(int2string(ninstaller-1) + 
+    else { printStatus(int2string(ninstaller-1) + 
                                    " dependencies for " + build.name() + "."); }
+  }
+  else
+  {
+    check = installer.create(build, _slackbuilds, action);
+    ninstaller = installer.numItems();
+  }
 
   installerwin = newwin(10, 10, 4, 4);
   installer.setWindow(installerwin);
@@ -1221,14 +1229,17 @@ void MainWindow::showBuildActions(BuildListItem & build)
     }
     else if ( (selected == "Install") || (selection == "I") ||
               (selected == "Upgrade") || (selection == "U") ||
-              (selected == "Reinstall") || (selection == "e") )
+              (selected == "Reinstall") || (selection == "e") ||
+              (selected == "Remove") || (selection == "R") )
     { 
       if ( (selected == "Install") || (selection == "I") ) 
         action = "Install";
       else if ( (selected == "Upgrade") || (selection == "U") )
         action = "Upgrade";
-      else 
+      else if ( (selected == "Reinstall") || (selection == "e") ) 
         action = "Reinstall";
+      else
+        action = "Remove";
 
       hideWindow(actionwin);
       redrawAll(true);

src/cpp/backend.cpp
@@ -136,23 +136,17 @@ std::vector<std::string> get_installed_info(const BuildListItem & build)
 {
   char buffer[128];
   FILE* fp;
-  std::string cmd, version, pkgname;
-  std::vector<std::string> output(2);
-  unsigned int i;
+  std::string cmd, pkg_info;
+  std::vector<std::string> output;
 
   cmd = env + sboutil + " get_installed_info " + build.name();
   fp = popen(cmd.c_str(), "r");
-  i = 0;
   while (fgets(buffer, sizeof(buffer), fp) != NULL) 
   { 
-    if (i == 0) { version = buffer; }
-    else { pkgname = buffer; }
-    i++;
+    pkg_info = buffer;
+    output.push_back(trim(pkg_info));
   }  
   pclose(fp);
-
-  output[0] = trim(version);
-  output[1] = trim(pkgname);
 
   return output;
 }
@@ -244,7 +238,7 @@ void list_installed(std::vector<BuildListItem> & slackbuilds,
         available_version = get_available_version(slackbuilds[i]);
         reqs = get_reqs(slackbuilds[i]);
         slackbuilds[i].setProp("installed_version", installed_version);
-        slackbuilds[i].setProp("package_name", installed_version);
+        slackbuilds[i].setProp("package_name", pkg_name);
         slackbuilds[i].setProp("available_version", available_version);
         slackbuilds[i].setProp("requires", reqs);
         installedlist.push_back(&slackbuilds[i]);

commit 9495679e8f7bc736af4810ed2e470e0b509e46e1

Fix bugs in InstallOrder::create introduced by 075652a (remove action)

src/cpp/InstallBox.cpp
@@ -364,17 +364,18 @@ int InstallBox::create(BuildListItem & build,
   // Set action for requested SlackBuild and add it at the end
 
   _builds.push_back(build);
-  _builds[nreqs].setBoolProp("tagged", true);
-  _builds[nreqs].addProp("action", action);
+  _builds[nbuilds].setBoolProp("tagged", true);
+  _builds[nbuilds].addProp("action", action);
+  nbuilds++;
 
   // Add to list (note have to do this separately because _builds changes
   // throughout the above loop)
 
-  for ( i = 0; i <= nreqs; i++ ) { addItem(&_builds[i]); }
+  for ( i = 0; i < nbuilds; i++ ) { addItem(&_builds[i]); }
 
   // Set window title
 
-  if (nreqs == 1)
+  if (nbuilds == 2)
   {
     if (action == "Remove")
       setName(build.name() + " (1 installed dep)");
@@ -384,9 +385,9 @@ int InstallBox::create(BuildListItem & build,
   else
   {
     if (action == "Remove")
-      setName(build.name() + " (" + int2string(nreqs) + " installed deps)");
+      setName(build.name() + " (" + int2string(nbuilds-1) + " installed deps)");
     else
-      setName(build.name() + " (" + int2string(nreqs) + " deps)");
+      setName(build.name() + " (" + int2string(nbuilds-1) + " deps)");
   }
 
   return check;