montagdude/sboui

commit c4a8dfe203d1330d35384e7a4f820342114e9430

Check if categories should be tagged after showBuildActions()

src/cpp/MainWindow.cpp
@@ -1380,10 +1380,9 @@ void MainWindow::showBuildActions(BuildListItem & build)
     else { getting_selection = false; }
   }
 
-  // Get rid of window and redraw
+  // Get rid of window (redraw happens in MainWindow::show())
 
   delwin(actionwin);
-  redrawAll(true);
 
   // Rebuild lists if SlackBuilds were installed/upgraded/reinstalled/removed
 
@@ -1404,6 +1403,7 @@ void MainWindow::show()
   std::string selection, statusmsg;
   bool getting_input, all_tagged;
   int check_quit;
+  unsigned int i, ncategories;
   BuildListItem build;
 
   redrawAll();
@@ -1525,6 +1525,16 @@ void MainWindow::show()
 
         _blistboxes[_category_idx].highlightedItem()->
                              setBoolProp("tagged", build.getBoolProp("tagged"));
+
+        // Determine if categories should be tagged and redraw
+
+        ncategories = _clistbox.numItems();
+        for ( i = 0; i < ncategories; i++ )
+        {
+          if (_blistboxes[i].allTagged())
+            _clistbox.itemByIdx(i)->setBoolProp("tagged", true);
+        }
+        redrawAll(true);
       }
     }
 

commit a6869a513ad92513e6d7c5212f279548a62aeb23

Fix potential segfault introduced by c4a8dfe203d1330d35384e7a4f820342114e9430
When highlightedItem() returns NULL, it would segfault due to trying to
call a method on a NULL pointer. Now, check for NULL first.

src/cpp/MainWindow.cpp
@@ -1405,6 +1405,7 @@ void MainWindow::show()
   int check_quit;
   unsigned int i, ncategories;
   BuildListItem build;
+  ListItem *item;
 
   redrawAll();
 
@@ -1523,8 +1524,8 @@ void MainWindow::show()
 
         // Update tag setting since we are working with a local copy
 
-        _blistboxes[_category_idx].highlightedItem()->
-                             setBoolProp("tagged", build.getBoolProp("tagged"));
+        if ( (item = _blistboxes[_category_idx].highlightedItem()) )
+          item->setBoolProp("tagged", build.getBoolProp("tagged"));
 
         // Determine if categories should be tagged and redraw
 