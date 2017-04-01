montagdude/sboui

commit ea1443ad55d5843c967a764950c93aa0a5f0c529

Organize _slackbuilds list into vector of vectors for each group
Simplifies logic in several parts of the code, increases efficiency a
little bit, and facilitates more efficient searches (to be implemented
later).

src/backend.cpp
@@ -24,7 +24,7 @@ Gets list of SlackBuilds by reading repo directory. Returns 0 if successful,
 1 if directory cannot be read.
 
 *******************************************************************************/
-int read_repo(std::vector<BuildListItem> & slackbuilds)
+int read_repo(std::vector<std::vector<BuildListItem> > & slackbuilds)
 {
   DirListing top_dir, category_dir;
   int stat;
@@ -45,6 +45,7 @@ int read_repo(std::vector<BuildListItem> & slackbuilds)
     cat_entry = top_dir(i);
     if (cat_entry.type == "dir")
     {
+      std::vector<BuildListItem> cat_builds;
       category_dir.setFromPath(cat_entry.path + "/" + cat_entry.name);
       nbuilds = category_dir.size();
       for ( j = 0; j < nbuilds; j++ )
@@ -55,10 +56,13 @@ int read_repo(std::vector<BuildListItem> & slackbuilds)
           BuildListItem build;
           build.setName(build_entry.name);
           build.setProp("category", cat_entry.name);
-          slackbuilds.push_back(build);
+          cat_builds.push_back(build);
         }
+        else { break; }   // Directories are listed first, so we're done
       }
+      slackbuilds.push_back(cat_builds);
     }
+    else { break; }       // Directories are listed first, so we're done
   }  
 
   return 0;
@@ -189,31 +193,35 @@ Populates list of installed SlackBuilds. Also determines installed version,
 available version, and dependencies for installed SlackBuilds.
 
 *******************************************************************************/
-void list_installed(std::vector<BuildListItem> & slackbuilds,
+void list_installed(std::vector<std::vector<BuildListItem> > & slackbuilds,
                     std::vector<BuildListItem *> & installedlist)
 {
   std::vector<std::string> installedpkgs;
   std::string pkgname, pkgversion, pkgbuild, pkgbuildnum;
-  unsigned int ninstalled, i, j, nbuilds;
+  unsigned int ninstalled, i, j, k, ncategories, nbuilds;
 
   installedlist.resize(0);
   installedpkgs = list_installed_packages();
   ninstalled = installedpkgs.size();
-  nbuilds = slackbuilds.size();
-  for ( j = 0; j < ninstalled; j++ )
+  ncategories = slackbuilds.size();
+  for ( k = 0; k < ninstalled; k++ )
   {
-    get_pkg_info(installedpkgs[j], pkgname, pkgversion, pkgbuildnum);
-    for ( i = 0; i < nbuilds; i++ )
+    get_pkg_info(installedpkgs[k], pkgname, pkgversion, pkgbuildnum);
+    for ( i = 0; i < ncategories; i++ )
     {
-      if (pkgname == slackbuilds[i].name())
-      {
-        slackbuilds[i].setBoolProp("installed", true);
-        slackbuilds[i].setProp("installed_version", pkgversion);
-        slackbuilds[i].setProp("package_name", installedpkgs[j]);
-        slackbuilds[i].setProp("package_build", pkgbuildnum);
-        slackbuilds[i].readPropsFromRepo();
-        installedlist.push_back(&slackbuilds[i]);
-        break;
+      nbuilds = slackbuilds[i].size();
+      for ( j = 0; j < nbuilds; j++ )
+      { 
+        if (pkgname == slackbuilds[i][j].name())
+        {
+          slackbuilds[i][j].setBoolProp("installed", true);
+          slackbuilds[i][j].setProp("installed_version", pkgversion);
+          slackbuilds[i][j].setProp("package_name", installedpkgs[i]);
+          slackbuilds[i][j].setProp("package_build", pkgbuildnum);
+          slackbuilds[i][j].readPropsFromRepo();
+          installedlist.push_back(&slackbuilds[i][j]);
+          break;
+        }
       }
     }
   } 

include/BuildOrderBox.h
@@ -41,7 +41,7 @@ class BuildOrderBox: public BuildListBox {
     /* Creates list based on SlackBuild selected */
 
     int create(BuildListItem & build,
-               std::vector<BuildListItem> & slackbuilds);
+               std::vector<std::vector<BuildListItem> > & slackbuilds);
 
     /* Draws frame, items, etc. as needed */
  

include/InstallBox.h
@@ -42,7 +42,7 @@ class InstallBox: public BuildOrderBox {
     /* Creates list based on SlackBuild selected */
 
     int create(BuildListItem & build,
-               std::vector<BuildListItem> & slackbuilds,
+               std::vector<std::vector<BuildListItem> > & slackbuilds,
                const std::string & action, bool resolve_deps=true,
                bool recheck=false);
 

include/MainWindow.h
@@ -28,7 +28,7 @@ class MainWindow {
     WINDOW *_win1, *_win2;
     CategoryListBox _clistbox;
     std::vector<BuildListBox> _blistboxes;
-    std::vector<BuildListItem> _slackbuilds;
+    std::vector<std::vector<BuildListItem> > _slackbuilds;
     std::vector<BuildListItem *> _installedlist, _nondeplist;
     std::vector<CategoryListItem> _categories;
     FilterBox _fbox;

include/backend.h
@@ -4,7 +4,7 @@
 #include <vector>
 #include "BuildListItem.h"
 
-int read_repo(std::vector<BuildListItem> & slackbuilds);
+int read_repo(std::vector<std::vector<BuildListItem> > & slackbuilds);
 std::vector<std::string> list_installed_packages();
 void get_pkg_info(const std::string & pkg, std::string & name,
                   std::string & version, std::string & buildnum);
@@ -15,7 +15,7 @@ bool check_installed(const BuildListItem & build,
 std::string get_reqs(const BuildListItem & build);
 void get_repo_info(const BuildListItem & build, std::string & available_version,
                    std::string & reqs);
-void list_installed(std::vector<BuildListItem> & slackbuilds,
+void list_installed(std::vector<std::vector<BuildListItem> > & slackbuilds,
                     std::vector<BuildListItem *> & installedlist);
 void list_nondeps(const std::vector<BuildListItem *> & installedlist,
                         std::vector<BuildListItem *> & nondeplist);

include/filters.h
@@ -7,7 +7,7 @@
 #include "CategoryListBox.h"
 #include "BuildListBox.h"
 
-void filter_all(std::vector<BuildListItem> & slackbuilds,
+void filter_all(std::vector<std::vector<BuildListItem> > & slackbuilds,
                        std::vector<CategoryListItem> & categories,
                        WINDOW *blistboxwin, CategoryListBox & clistbox,
                        std::vector<BuildListBox> & blistboxes);
@@ -20,7 +20,7 @@ void filter_upgradable(std::vector<BuildListItem *> & installedlist,
                        WINDOW *blistboxwin, CategoryListBox & clistbox,
                        std::vector<BuildListBox> & blistboxes,
                        unsigned int & nupgradable);
-void filter_tagged(std::vector<BuildListItem> & slackbuilds,
+void filter_tagged(std::vector<std::vector<BuildListItem> > & slackbuilds,
                        std::vector<CategoryListItem> & categories,
                        WINDOW *blistboxwin, CategoryListBox & clistbox,
                        std::vector<BuildListBox> & blistboxes,
@@ -29,7 +29,7 @@ void filter_nondeps(std::vector<BuildListItem *> & nondeplist,
                        std::vector<CategoryListItem> & categories,
                        WINDOW *blistboxwin, CategoryListBox & clistbox,
                        std::vector<BuildListBox> & blistboxes);
-void filter_search(std::vector<BuildListItem> & slackbuilds,
+void filter_search(std::vector<std::vector<BuildListItem> > & slackbuilds,
                        std::vector<CategoryListItem> & categories,
                        WINDOW *blistboxwin, CategoryListBox & clistbox,
                        std::vector<BuildListBox> & blistboxes,

include/requirements.h
@@ -5,7 +5,7 @@
 
 int compute_reqs_order(const BuildListItem & build,
                        std::vector<BuildListItem *> & reqlist,
-                       std::vector<BuildListItem> & slackbuilds);
+                       std::vector<std::vector<BuildListItem> > & slackbuilds);
 void compute_inv_reqs(const BuildListItem & build,
                       std::vector<BuildListItem *> & invreqlist,
                       std::vector<BuildListItem *> & installedlist);

src/BuildOrderBox.cpp
@@ -281,7 +281,7 @@ succeeded or 1 if some could not be found in the repository.
 
 *******************************************************************************/
 int BuildOrderBox::create(BuildListItem & build,
-                          std::vector<BuildListItem> & slackbuilds) 
+                         std::vector<std::vector<BuildListItem> > & slackbuilds)
 {
   int check; 
   unsigned int nbuilds, i;

commit f80323d57b915fc8aa1c57fad4265a9f07e280f5

Fix bug introduced by ea1443a which would remove the wrong package
Woops!

src/backend.cpp
@@ -316,7 +316,7 @@ void list_installed(std::vector<std::vector<BuildListItem> > & slackbuilds,
     {
       slackbuilds[i][j].setBoolProp("installed", true);
       slackbuilds[i][j].setProp("installed_version", pkgversion);
-      slackbuilds[i][j].setProp("package_name", installedpkgs[i]);
+      slackbuilds[i][j].setProp("package_name", installedpkgs[k]);
       slackbuilds[i][j].setProp("package_build", pkgbuildnum);
       slackbuilds[i][j].readPropsFromRepo();
       installedlist.push_back(&slackbuilds[i][j]);