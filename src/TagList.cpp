montagdude/sboui

commit bb4669887154df0e2c5d8037bac9419ad036197f

Use SlackBuild pointer/reference instead of copy in most cases
Applies to showBuildActions, TagList, printPackageVersion. Removes the
need for an explicit ListItem to BuildListItem conversion, and
will allow automatic update of install status when applying changes.

src/TagList.cpp
@@ -56,7 +56,7 @@ void TagList::redrawSingleItem(unsigned int idx)
 
   // Print item with selection
 
-  if (_items[idx]->getBoolProp("tagged")) { wprintw(_win, "[X] "); }
+  if (_items[idx]->getBoolProp("marked")) { wprintw(_win, "[X] "); }
   else { wprintw(_win, "[ ] "); }
 
   printToEol(_items[idx]->name());
@@ -91,7 +91,12 @@ TagList::TagList(WINDOW *win, const std::string & name)
 Edit list items
 
 *******************************************************************************/
-void TagList::addItem(ListItem *item) { _tagged.push_back(*item); }
+void TagList::addItem(ListItem *item)
+{
+  item->setBoolProp("marked", true);
+  _tagged.push_back(item);
+}
+
 void TagList::removeItem(ListItem *item)
 {
   unsigned int i, ntagged;
@@ -99,7 +104,7 @@ void TagList::removeItem(ListItem *item)
   ntagged = _tagged.size();
   for ( i = 0; i < ntagged; i++ )
   {
-    if (_tagged[i].name() == item->name())
+    if (_tagged[i]->name() == item->name())
     {
       _tagged.erase(_tagged.begin()+i); 
       break;
@@ -147,7 +152,7 @@ unsigned int TagList::getDisplayList(const std::string & action)
 {
   unsigned int i, ntagged;
   bool add_item;
-  BuildListItem item;
+  BuildListItem *item;
 
   _items.resize(0);
   
@@ -155,24 +160,24 @@ unsigned int TagList::getDisplayList(const std::string & action)
   for ( i = 0; i < ntagged; i++ )
   {
     add_item = false;
-    item = _tagged[i];
+    item = static_cast<BuildListItem *>(_tagged[i]);
     if (action == "Install")
     {
-      if (! item.getBoolProp("installed")) { add_item = true; }
+      if (! item->getBoolProp("installed")) { add_item = true; }
     }
     else if (action == "Upgrade")
     {
-      if ( (item.getBoolProp("upgradable")) &&
-           (! item.getBoolProp("blacklisted")) )
+      if ( (item->getBoolProp("upgradable")) &&
+           (! item->getBoolProp("blacklisted")) )
         add_item = true;
     }
     else if ( (action == "Remove") || (action == "Reinstall") )
     {
-      if ( (item.getBoolProp("installed")) &&
-           (! item.getBoolProp("blacklisted")) ) { add_item = true; }
+      if ( (item->getBoolProp("installed")) &&
+           (! item->getBoolProp("blacklisted")) ) { add_item = true; }
     }
        
-    if (add_item) { _items.push_back(&_tagged[i]); }
+    if (add_item) { _items.push_back(_tagged[i]); }
   }
 
   return numItems();
@@ -271,8 +276,8 @@ std::string TagList::exec()
 
     case ' ':
       retval = " ";
-      _items[_highlight]->setBoolProp("tagged", 
-                                 (! _items[_highlight]->getBoolProp("tagged")));
+      _items[_highlight]->setBoolProp("marked", 
+                                 (! _items[_highlight]->getBoolProp("marked")));
       check_redraw = highlightNext();
       if (check_redraw == 1) { _redraw_type = "all"; }
       else { _redraw_type = "changed"; }
@@ -290,24 +295,8 @@ std::string TagList::exec()
 
 /*******************************************************************************
 
-Converts item in _items or _tagged list to BuildListItem. Careful: does not 
-perform bounds checks!
+Returns item from _tagged vector by index. Careful: does not perform bounds
+checks!
 
 *******************************************************************************/
-BuildListItem TagList::itemByIdx(unsigned int idx) const
-{
-  BuildListItem item;
-
-  item = *_items[idx];
-
-  return item;
-}
-
-BuildListItem TagList::taggedByIdx(unsigned int idx) const
-{
-  BuildListItem item;
-
-  item = _tagged[idx];
-
-  return item;
-}
+ListItem * TagList::taggedByIdx(unsigned int idx) { return _tagged[idx]; }

include/BuildActionBox.h
@@ -17,9 +17,6 @@ class BuildActionBox: public SelectionBox {
     /* Constructors */
 
     BuildActionBox();
-    BuildActionBox(WINDOW *win, const std::string & name);
-    BuildActionBox(WINDOW *win, const std::string & name,
-                   const BuildListItem & build);
 
     /* Destructor */
 
@@ -27,5 +24,5 @@ class BuildActionBox: public SelectionBox {
 
     /* Sets up actions based on the SlackBuild selected */
 
-    void create(const BuildListItem & build);
+    void create(BuildListItem & build);
 };

include/TagList.h
@@ -20,7 +20,7 @@ class TagList: public SelectionBox {
        showing only what is appropriate for the requested action (install,
        upgrade, etc.). */
 
-    std::vector<ListItem> _tagged;
+    std::vector<ListItem *> _tagged;
 
     /* Drawing */
     
@@ -52,8 +52,7 @@ class TagList: public SelectionBox {
 
     std::string exec();
 
-    /* Returns BuildListItem by index in _items or _tagged list */
+    /* Returns pointer to item by index in _tagged list */
 
-    BuildListItem itemByIdx(unsigned int idx) const;
-    BuildListItem taggedByIdx(unsigned int idx) const;
+    ListItem * taggedByIdx(unsigned int idx);
 };

src/BuildActionBox.cpp
@@ -7,29 +7,13 @@
 
 /*******************************************************************************
 
-Constructors
+Constructor
 
 *******************************************************************************/
 BuildActionBox::BuildActionBox()
 {
   _name = "Select an action";
   _info = "Enter: Ok | Esc: Cancel";
-}
-
-BuildActionBox::BuildActionBox(WINDOW *win, const std::string & name)
-{
-  _win = win;
-  _name = name;
-  _info = "Enter: Ok | Esc: Cancel";
-}
-
-BuildActionBox::BuildActionBox(WINDOW *win, const std::string & name,
-                               const BuildListItem & build)
-{
-  _win = win;
-  _name = name;
-  _info = "Enter: Ok | Esc: Cancel";
-  create(build);
 }
 
 /*******************************************************************************
@@ -51,7 +35,7 @@ BuildActionBox::~BuildActionBox()
 Creates options using info about SlackBuild
 
 *******************************************************************************/
-void BuildActionBox::create(const BuildListItem & build)
+void BuildActionBox::create(BuildListItem & build)
 {
   unsigned int count;
 

src/BuildListItem.cpp
@@ -65,60 +65,6 @@ BuildListItem::BuildListItem()
 
 /*******************************************************************************
 
-Converts ListItem to BuildListItem
-
-*******************************************************************************/
-void BuildListItem::operator = (const ListItem & item)
-{
-  _name = item.name();
-
-  if (item.checkProp("category"))
-    setProp("category", item.getProp("category"));
-  else { addProp("category", ""); }
-
-  if (item.checkProp("installed_version"))
-    setProp("installed_version", item.getProp("installed_version"));
-  else { addProp("installed_version", ""); }
-
-  if (item.checkProp("available_version"))
-    setProp("available_version", item.getProp("available_version"));
-  else { addProp("available_version", ""); }
-
-  if (item.checkProp("requires"))
-    setProp("requires", item.getProp("requires"));
-  else { addProp("requires", ""); }
-
-  if (item.checkProp("installed"))
-    setBoolProp("installed", item.getBoolProp("installed"));
-  else { addBoolProp("installed", false); }
-
-  if (item.checkProp("upgradable"))
-    setBoolProp("upgradable", item.getBoolProp("upgradable"));
-  else { addBoolProp("upgradable", false); }
-
-  if (item.checkProp("blacklisted"))
-    setBoolProp("blacklisted", item.getBoolProp("blacklisted"));
-  else { addBoolProp("blacklisted", false); }
-
-  if (item.checkProp("tagged"))
-    setBoolProp("tagged", item.getBoolProp("tagged"));
-  else { addBoolProp("tagged", false); }
-
-  if (item.checkProp("marked"))
-    setBoolProp("marked", item.getBoolProp("marked"));
-  else { addBoolProp("marked", false); }
-
-  if (item.checkProp("package_name"))
-    setProp("package_name", item.getProp("package_name"));
-  else { addProp("package_name", ""); }
-
-  if (item.checkProp("action"))
-    setProp("action", item.getProp("action"));
-  else { addProp("action", ""); }
-}
-
-/*******************************************************************************
-
 Checks whether this BuildListItem is installed and gets information about it
 if so. If repo info has been read already, checks whether installed SlackBuild
 is also upgradable.

src/InstallBox.cpp
@@ -357,6 +357,7 @@ int InstallBox::create(BuildListItem & build,
   nbuilds = 0;
   for ( i = 0; i <= nreqs; i++ )
   {
+    mark = false;
     if (action != "Remove")
     {
       _builds.push_back(reqlist[i]);

src/MainWindow.cpp
@@ -285,17 +285,18 @@ Rebuilds lists after applying changes
 void MainWindow::rebuild()
 {
   unsigned int k;
-  int i, j, ntagged;
+  int ntagged;
   std::vector<std::string> pkg_errors, missing_info;
   std::string errmsg;
+  BuildListItem *build;
 
   // Clear tags
 
   ntagged = _taglist.numTagged();
   for ( k = 0; int(k) < ntagged; k++ )
   {
-    find_slackbuild(_taglist.taggedByIdx(k).name(), _slackbuilds, i, j);
-    _slackbuilds[i][j].setBoolProp("tagged", false);
+    build = static_cast<BuildListItem *>(_taglist.taggedByIdx(k));
+    build->setBoolProp("tagged", false);
   }
   _taglist.clearList();
 
@@ -1080,7 +1081,7 @@ void MainWindow::applyTags(const std::string & action)
   int ninstalled, nupgraded, nreinstalled, nremoved;
   bool getting_input, cancel_all, apply_changes, any_modified, needs_rebuild;
   std::string selection;
-  BuildListItem item;
+  BuildListItem *build;
 
   ndisplay = _taglist.getDisplayList(action);
   if (ndisplay == 0)
@@ -1137,14 +1138,14 @@ void MainWindow::applyTags(const std::string & action)
     nremoved = 0;
     for ( i = 0; i < ndisplay; i++ ) 
     {
-      item = _taglist.itemByIdx(i);
-      if (item.getBoolProp("tagged"))
+      build = static_cast<BuildListItem *>(_taglist.itemByIdx(i));
+      if (build->getBoolProp("marked"))
       { 
-        any_modified = modifyPackage(item, action, ninstalled, nupgraded,
+        any_modified = modifyPackage(*build, action, ninstalled, nupgraded,
                                      nreinstalled, nremoved, cancel_all, true);
         if (! needs_rebuild) { needs_rebuild = any_modified; }
+        draw(true);
       }
-      draw(true);
       if (cancel_all) { break; }
     }
     if (needs_rebuild)
@@ -1685,7 +1686,7 @@ void MainWindow::quickSearch()
   QuickSearch qsearch;
   bool searching;
   std::string selection, entry;
-  BuildListItem build;
+  BuildListItem *build;
 
   getmaxyx(stdscr, rows, cols);
 
@@ -1727,8 +1728,9 @@ void MainWindow::quickSearch()
 
   if (_activated_listbox == 1)
   {
-    build = *_blistboxes[_category_idx].highlightedItem();
-    printPackageVersion(build);
+    build = static_cast<BuildListItem *>(
+                                  _blistboxes[_category_idx].highlightedItem());
+    printPackageVersion(*build);
   }
   else { clearStatus(); }
 }
@@ -1768,8 +1770,7 @@ std::string MainWindow::exec()
   bool getting_input, all_tagged;
   int check_quit;
   unsigned int i, ncategories;
-  BuildListItem build;
-  ListItem *item;
+  BuildListItem *build;
 
   draw();
 
@@ -1803,9 +1804,12 @@ std::string MainWindow::exec()
         _activated_listbox = 1;
 
         // Display status message for installed SlackBuild
+        // Note on static cast: allows the original BuildListItem pointer to
+        // be referenced.
 
-        build = *_blistboxes[_category_idx].highlightedItem();
-        printPackageVersion(build);
+        build = static_cast<BuildListItem *>(
+                                  _blistboxes[_category_idx].highlightedItem());
+        printPackageVersion(*build);
       }
 
       // Tag signal: tag/untag all items in category
@@ -1830,8 +1834,9 @@ std::string MainWindow::exec()
       {
         // Display status message for installed SlackBuild
 
-        build = *_blistboxes[_category_idx].highlightedItem();
-        printPackageVersion(build);
+        build = static_cast<BuildListItem *>(
+                                  _blistboxes[_category_idx].highlightedItem());
+        printPackageVersion(*build);
       }
 
       // Tab signal or left key
@@ -1873,13 +1878,9 @@ std::string MainWindow::exec()
 
       else if (selection == signals::keyEnter)
       {
-        build = *_blistboxes[_category_idx].highlightedItem();
-        showBuildActions(build);
-
-        // Update tag setting since we are working with a local copy
-
-        if ( (item = _blistboxes[_category_idx].highlightedItem()) )
-          item->setBoolProp("tagged", build.getBoolProp("tagged"));
+        build = static_cast<BuildListItem *>(
+                                  _blistboxes[_category_idx].highlightedItem());
+        showBuildActions(*build);
 
         // Determine if categories should be tagged and redraw
 

commit f5d68859636c8014823571b025805db58ecd7ec5

Fix marking instead of tagging bug introduced by bb466988

src/TagList.cpp
@@ -93,7 +93,7 @@ Edit list items
 *******************************************************************************/
 void TagList::addItem(ListItem *item)
 {
-  item->setBoolProp("marked", true);
+  item->setBoolProp("tagged", true);
   _tagged.push_back(item);
 }
 