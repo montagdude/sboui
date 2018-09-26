montagdude/sboui

commit bcb45467687ba727d1859a4c8a3cb28697f3c996

Mouse support for BuildActionBox + scroll by click position at right
ListBoxes still need awareness of click on Ok/Cancel buttons. Also
changed scroll position indicator to be based on current highlight
rather than current page -> more intuitive for clicking position at
right of ListBox to scroll. Some other small improvements to redrawing
logic were also made.

include/AbstractListBox.h
@@ -16,6 +16,7 @@ class AbstractListBox: public CursesWidget {
 
   protected:
 
+    int _highlight, _prevhighlight;
     std::string _name, _redraw_type;
     std::vector<ListItem *> _items;
     unsigned int _reserved_rows, _header_rows;

include/ListBox.h
@@ -16,24 +16,24 @@ class ListBox: public AbstractListBox {
 
   protected:
 
-    int _highlight, _prevhighlight;
     bool _activated;
 
     /* Setting item to be highlighted */
-  
+
     int highlightFirst();
     int highlightLast();
     int highlightPrevious();
     int highlightNext();
     int highlightNextPage();
     int highlightPreviousPage();
-                   
+    int highlightFractional(const double & frac);
+
     /* Determines first item to print (needed for scrolling) */
-    
+
     int determineFirstPrint();
 
     /* Drawing */
-    
+
     virtual void redrawFrame() const;
     virtual void redrawSingleItem(unsigned int idx);
     void redrawChangedItems();
@@ -74,7 +74,7 @@ class ListBox: public AbstractListBox {
 
     /* Handles mouse event */
 
-    std::string handleMouseEvent(const MouseEvent * mevent);
+    std::string handleMouseEvent(MouseEvent * mevent);
 
     /* Draws frame, items, etc. as needed */
  

include/ScrollBox.h
@@ -43,7 +43,7 @@ class ScrollBox: public AbstractListBox {
 
     /* Mouse interaction */
 
-    std::string handleMouseEvent(const MouseEvent * mevent);
+    std::string handleMouseEvent(MouseEvent * mevent);
 
     /* Draws frame, items, etc. as needed */
  

src/AbstractListBox.cpp
@@ -1,6 +1,6 @@
 #include <string>
 #include <curses.h>
-#include <cmath>     // floor
+#include <cmath>     // floor, ceil
 #include "ListItem.h"
 #include "AbstractListBox.h"
 
@@ -11,7 +11,8 @@ Redraws right border between header and footer and scroll indicators
 *******************************************************************************/
 void AbstractListBox::redrawScrollIndicator() const
 {
-  int rows, cols, i, rowsavail, maxscroll, pos;
+  int rows, cols, i, rowsavail, pos;
+  double frac;
   bool need_up, need_dn;
 
   // Check if a scroll indicator is needed
@@ -38,10 +39,17 @@ void AbstractListBox::redrawScrollIndicator() const
 
   // Draw position indicator
 
-  if ( (need_up) || (need_dn) )
+  if ( (_items.size() > 0) && ((need_up) || (need_dn)) )
   {
-    maxscroll = _items.size() - rowsavail; 
-    pos = std::floor(double(_firstprint)/double(maxscroll)*(rowsavail-1));
+    frac = double(_highlight)/double(_items.size()-1);
+
+    // Make sure we don't indicate the top or bottom before we're really there
+
+    if (frac < 0.5)
+      pos = std::ceil(frac*(rowsavail-1));
+    else
+      pos = std::floor(frac*(rowsavail-1));
+
     mvwaddch(_win, _header_rows+pos, cols-1, ACS_DIAMOND);
   }
 }
@@ -54,6 +62,8 @@ Constructors
 AbstractListBox::AbstractListBox()
 {
   _name = "";
+  _highlight = 0;
+  _prevhighlight = 0;
   _redraw_type = "all";
   _items.resize(0);
   _firstprint = 0;
@@ -65,6 +75,8 @@ AbstractListBox::AbstractListBox(WINDOW *win, const std::string & name)
 {
   _win = win;
   _name = name;
+  _highlight = 0;
+  _prevhighlight = 0;
   _redraw_type = "all";
   _items.resize(0);
   _firstprint = 0;

src/ListBox.cpp
@@ -23,6 +23,7 @@ int ListBox::highlightFirst()
 
   if (_items.size() == 0) { return 0; }
 
+  _prevhighlight = _highlight;
   _highlight = 0;
   if (_firstprint == 0) { retval = 0; }
   else { retval = 1; }
@@ -41,6 +42,7 @@ int ListBox::highlightLast()
 {
   if (_items.size() == 0) { return 0; }
 
+  _prevhighlight = _highlight;
   _highlight = std::max(int(_items.size()) - 1, 0);
   return determineFirstPrint();
 }
@@ -58,6 +60,7 @@ int ListBox::highlightPrevious()
   if (_highlight == 0) { return 0; }
   else
   {
+    _prevhighlight = _highlight;
     _highlight -= 1;
     return determineFirstPrint();
   }
@@ -76,6 +79,7 @@ int ListBox::highlightNext()
   if (_highlight == int(_items.size())-1) { return 0; }
   else
   {
+    _prevhighlight = _highlight;
     _highlight += 1;
     return determineFirstPrint();
   }
@@ -109,6 +113,7 @@ int ListBox::highlightNextPage()
 
   // Determine which choice to highlight
 
+  _prevhighlight = _highlight;
   _highlight += rowsavail;
 
   return 1;
@@ -140,9 +145,27 @@ int ListBox::highlightPreviousPage()
 
   // Determine which choice to highlight
 
+  _prevhighlight = _highlight;
   _highlight -= rowsavail;
 
   return 1;
+}
+
+/*******************************************************************************
+
+Scrolls to a page based on a fractional value of all items in the list. Return
+value of 0 means that _firstprint hasn't changed; 1 means it has.
+
+*******************************************************************************/
+int ListBox::highlightFractional(const double & frac)
+{
+  if (_items.size() == 0) { return 0; }
+  if (frac < 0.) { return 0; }
+  else if (frac > 1.) { return 0; }
+
+  _prevhighlight = _highlight;
+  _highlight = std::floor(frac*double(_items.size()-1));
+  return determineFirstPrint();
 }
 
 /*******************************************************************************
@@ -348,8 +371,6 @@ Constructors
 *******************************************************************************/
 ListBox::ListBox()
 {
-  _highlight = 0;
-  _prevhighlight = 0;
   _activated = true;
 }
 
@@ -357,8 +378,6 @@ ListBox::ListBox(WINDOW *win, const std::string & name)
 {
   _win = win;
   _name = name;
-  _highlight = 0;
-  _prevhighlight = 0;
   _activated = true;
 }
 
@@ -517,9 +536,10 @@ ListItem * ListBox::highlightedItem()
 Handles mouse events
 
 *******************************************************************************/
-std::string ListBox::handleMouseEvent(const MouseEvent * mevent)
+std::string ListBox::handleMouseEvent(MouseEvent * mevent)
 {
   int rows, cols, begy, begx, ycurs, xcurs, rowsavail, check_redraw;
+  double frac;
 
   if (_items.size() == 0)
     return signals::nullEvent;
@@ -537,7 +557,7 @@ std::string ListBox::handleMouseEvent(const MouseEvent * mevent)
     else if ( (xcurs < 1) || (xcurs >= cols) )
       return signals::nullEvent;
 
-    // Check for clicking scroll arrows
+    // Check for clicking on scroll area
 
     else if (xcurs == cols-1)
     {
@@ -550,7 +570,6 @@ std::string ListBox::handleMouseEvent(const MouseEvent * mevent)
             _redraw_type = "all";
           else
             _redraw_type = "changed";
-          draw();
           return signals::highlight;
         }
         else
@@ -565,14 +584,22 @@ std::string ListBox::handleMouseEvent(const MouseEvent * mevent)
             _redraw_type = "all";
           else
             _redraw_type = "changed";
-          draw();
           return signals::highlight;
         }
         else
           return signals::nullEvent;
       }
       else
-        return signals::nullEvent;
+      {
+        frac = double(ycurs-_header_rows) /
+               double(rowsavail-1);
+        check_redraw = highlightFractional(frac);
+        if (check_redraw == 1)
+          _redraw_type = "all";
+        else
+          _redraw_type = "changed";
+        return signals::highlight;
+      }
     }
 
     // Clicked on empty space in the list box
@@ -590,7 +617,6 @@ std::string ListBox::handleMouseEvent(const MouseEvent * mevent)
         _redraw_type = "all";
       else
         _redraw_type = "changed";
-      draw();
 
       if (mevent->doubleClick())
         return signals::keyEnter;
@@ -652,7 +678,7 @@ std::string ListBox::exec(MouseEvent * mevent)
 
   // Highlight first entry on first display
 
-  if (_highlight == 0) { highlightFirst(); }
+  if ( (_highlight == 0) && (_prevhighlight == 0) ) { highlightFirst(); }
 
   // Draw list elements
 

src/ScrollBox.cpp
@@ -296,7 +296,7 @@ void ScrollBox::preferredSize(int & height, int & width) const
 Handles mouse events
 
 *******************************************************************************/
-std::string ScrollBox::handleMouseEvent(const MouseEvent * event)
+std::string ScrollBox::handleMouseEvent(MouseEvent * event)
 {
   //FIXME: implement
   return signals::nullEvent;

include/CursesWidget.h
@@ -46,7 +46,7 @@ class CursesWidget {
 
     /* Mouse interaction */
 
-    virtual std::string handleMouseEvent(const MouseEvent * mevent) = 0;
+    virtual std::string handleMouseEvent(MouseEvent * mevent) = 0;
 
     /* Draws frame and message */
 

include/InputBox.h
@@ -72,7 +72,7 @@ class InputBox: public CursesWidget {
 
     /* Mouse interaction */
 
-    std::string handleMouseEvent(const MouseEvent * mevent);
+    std::string handleMouseEvent(MouseEvent * mevent);
 
     /* Draws frame, entry, etc. as needed */
 

commit 7358a5c443d161d448de244758d547534eee7b1f

Fix scroll inidicator in ScrollBox
It was broken by bcb45467687ba727d1859a4c8a3cb28697f3c996. This commit
reverts it to the old behavior, but keeps the new (selection-based
instead of page-based) behavior for ListBoxes.

include/AbstractListBox.h
@@ -9,14 +9,14 @@ class ListItem;
 
 /*******************************************************************************
 
-Template list box class
+Template list box class. Used by both ListBox class (selectable items) and
+ScrollBox class (non-selectable items).
 
 *******************************************************************************/
 class AbstractListBox: public CursesWidget {
 
   protected:
 
-    int _highlight, _prevhighlight;
     std::string _name, _redraw_type;
     std::vector<ListItem *> _items;
     unsigned int _reserved_rows, _header_rows;
@@ -24,7 +24,7 @@ class AbstractListBox: public CursesWidget {
 
     virtual void redrawFrame() = 0;
     virtual void redrawSingleItem(unsigned int idx) = 0;
-    virtual void redrawScrollIndicator() const;
+    virtual void redrawScrollIndicator() const = 0;
 
   public:
 

include/ListBox.h
@@ -15,8 +15,12 @@ List box with selectable items
 *******************************************************************************/
 class ListBox: public AbstractListBox {
 
+//FIXME: move as much of this stuff as possible up to AbstractListBox so that
+// it can be reused by ScrollBox.
+
   protected:
 
+    int _highlight, _prevhighlight;
     bool _activated;
     std::vector<std::string> _buttons;  // At the bottom, e.g. OK/Cancel
     std::vector<std::string> _button_signals;
@@ -46,6 +50,7 @@ class ListBox: public AbstractListBox {
     virtual void redrawFrame();
     virtual void redrawButtons();
     virtual void redrawSingleItem(unsigned int idx);
+    virtual void redrawScrollIndicator() const;
     void redrawChangedItems();
     void redrawAllItems();
 

include/ScrollBox.h
@@ -7,7 +7,7 @@
 
 /*******************************************************************************
 
-Same idea as a ListBox, but items are not selectable/highlightable
+Same idea as a ListBox, but items are not selectable/highlightable.
 
 *******************************************************************************/
 class ScrollBox: public AbstractListBox {
@@ -27,6 +27,7 @@ class ScrollBox: public AbstractListBox {
     
     virtual void redrawFrame();
     virtual void redrawSingleItem(unsigned int idx);
+    virtual void redrawScrollIndicator() const;
     void redrawAllItems();
 
   public:

src/AbstractListBox.cpp
@@ -6,64 +6,12 @@
 
 /*******************************************************************************
 
-Redraws right border between header and footer and scroll indicators
-
-*******************************************************************************/
-void AbstractListBox::redrawScrollIndicator() const
-{
-  int rows, cols, i, rowsavail, pos;
-  double frac;
-  bool need_up, need_dn;
-
-  // Check if a scroll indicator is needed
-
-  getmaxyx(_win, rows, cols);
-  rowsavail = rows-_reserved_rows;
-
-  need_up = false;
-  need_dn = false;
-  if (_firstprint != 0) { need_up = true; }
-  if (_items.size() > _firstprint + rows-_reserved_rows) { need_dn = true; }
-
-  // Draw right border
-
-  for ( i = _header_rows; i < int(_header_rows)+rowsavail; i++ )
-  {
-    mvwaddch(_win, i, cols-1, ACS_VLINE);
-  }
-
-  // Draw up and down arrows
-
-  if (need_up) { mvwaddch(_win, _header_rows, cols-1, ACS_UARROW); }
-  if (need_dn) { mvwaddch(_win, _header_rows+rowsavail-1, cols-1, ACS_DARROW); }
-
-  // Draw position indicator
-
-  if ( (_items.size() > 0) && ((need_up) || (need_dn)) )
-  {
-    frac = double(_highlight)/double(_items.size()-1);
-
-    // Make sure we don't indicate the top or bottom before we're really there
-
-    if (frac < 0.5)
-      pos = std::ceil(frac*(rowsavail-1));
-    else
-      pos = std::floor(frac*(rowsavail-1));
-
-    mvwaddch(_win, _header_rows+pos, cols-1, ACS_DIAMOND);
-  }
-}
-
-/*******************************************************************************
-
 Constructors
 
 *******************************************************************************/
 AbstractListBox::AbstractListBox()
 {
   _name = "";
-  _highlight = 0;
-  _prevhighlight = 0;
   _redraw_type = "all";
   _items.resize(0);
   _firstprint = 0;
@@ -75,8 +23,6 @@ AbstractListBox::AbstractListBox(WINDOW *win, const std::string & name)
 {
   _win = win;
   _name = name;
-  _highlight = 0;
-  _prevhighlight = 0;
   _redraw_type = "all";
   _items.resize(0);
   _firstprint = 0;

src/ListBox.cpp
@@ -434,6 +434,57 @@ void ListBox::redrawSingleItem(unsigned int idx)
 
 /*******************************************************************************
 
+Redraws right border between header and footer and scroll indicators
+
+*******************************************************************************/
+void ListBox::redrawScrollIndicator() const
+{
+  int rows, cols, i, rowsavail, pos;
+  double frac;
+  bool need_up, need_dn;
+
+  // Check if a scroll indicator is needed
+
+  getmaxyx(_win, rows, cols);
+  rowsavail = rows-_reserved_rows;
+
+  need_up = false;
+  need_dn = false;
+  if (_firstprint != 0) { need_up = true; }
+  if (_items.size() > _firstprint + rows-_reserved_rows) { need_dn = true; }
+
+  // Draw right border
+
+  for ( i = _header_rows; i < int(_header_rows)+rowsavail; i++ )
+  {
+    mvwaddch(_win, i, cols-1, ACS_VLINE);
+  }
+
+  // Draw up and down arrows
+
+  if (need_up) { mvwaddch(_win, _header_rows, cols-1, ACS_UARROW); }
+  if (need_dn) { mvwaddch(_win, _header_rows+rowsavail-1, cols-1, ACS_DARROW); }
+
+  // Draw position indicator
+
+  if ( (_items.size() > 0) && ((need_up) || (need_dn)) )
+  {
+    frac = double(_highlight)/double(_items.size()-1);
+
+    // Make sure we don't indicate the top or bottom before we're really there
+
+    if (frac < 0.5)
+      pos = std::ceil(frac*(rowsavail-1));
+    else
+      pos = std::floor(frac*(rowsavail-1));
+
+    mvwaddch(_win, _header_rows+pos, cols-1, ACS_DIAMOND);
+  }
+}
+
+
+/*******************************************************************************
+
 Redraws the previously and currently highlighted items
 
 *******************************************************************************/

src/ScrollBox.cpp
@@ -191,6 +191,50 @@ void ScrollBox::redrawFrame()
 }
 
 /*******************************************************************************
+
+Redraws scroll indicator. Differs from regular list box scroll indicator in that
+it is based on pages rather than highlighted item (there is no highlighted item
+in this class).
+
+*******************************************************************************/
+void ScrollBox::redrawScrollIndicator() const
+{
+  int rows, cols, i, rowsavail, maxscroll, pos;
+  bool need_up, need_dn;
+
+  // Check if a scroll indicator is needed
+
+  getmaxyx(_win, rows, cols);
+  rowsavail = rows-_reserved_rows;
+
+  need_up = false;
+  need_dn = false;
+  if (_firstprint != 0) { need_up = true; }
+  if (_items.size() > _firstprint + rows-_reserved_rows) { need_dn = true; }
+
+  // Draw right border
+
+  for ( i = _header_rows; i < int(_header_rows)+rowsavail; i++ )
+  {
+    mvwaddch(_win, i, cols-1, ACS_VLINE);
+  }
+
+  // Draw up and down arrows
+
+  if (need_up) { mvwaddch(_win, _header_rows, cols-1, ACS_UARROW); }
+  if (need_dn) { mvwaddch(_win, _header_rows+rowsavail-1, cols-1, ACS_DARROW); }
+
+  // Draw position indicator
+
+  if ( (need_up) || (need_dn) )
+  {
+    maxscroll = _items.size() - rowsavail;
+    pos = std::floor(double(_firstprint)/double(maxscroll)*(rowsavail-1));
+    mvwaddch(_win, _header_rows+pos, cols-1, ACS_DIAMOND);
+  }
+}
+
+/*******************************************************************************
  
 Redraws a single item. Note: doesn't check if the item is actually on the
 screen or not.